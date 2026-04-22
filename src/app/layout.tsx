596654be55b3c6fe53d5ebd9b7e56ef637061b2b
maebahesioru <pikomaeba@gmail.com>
2026-04-22 17:16:26 +0900

    fix: htmlタグにsuppressHydrationWarning追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: `
           (function(){
